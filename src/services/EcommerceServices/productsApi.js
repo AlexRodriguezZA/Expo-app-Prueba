b317dfc436d4573ba0c5eb131f473798b57e95ee
AlexRodriguezZA <alekeyn1999@gmail.com>
2024-03-12 15:01:50 -0300

    Resolviendo deploy

--- src/services/EcommerceServices/productsApi.js
+++ src/services/EcommerceServices/productsApi.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const productsApi = async () => {
   try {
     const response = await axios.get('https://fakestoreapi.com/products');
-    return response.data;
+    return response.data; 
   } catch (error) {
     throw new Error('Error al traer los productos');
   }
